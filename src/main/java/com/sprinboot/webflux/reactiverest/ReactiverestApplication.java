faf57245284a4b0d5c5b44305ddfab2f6f88bc95
yanad <yana.danilova1509@gmail.com>
2023-08-10 13:37:10 +0200

    Initial commit

---
+++ src/main/java/com/sprinboot/webflux/reactiverest/ReactiverestApplication.java
@@ -0,0 +1,13 @@
+package com.sprinboot.webflux.reactiverest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiverestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiverestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprinboot/webflux/reactiverest/ReactiverestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinboot.webflux.reactiverest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiverestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
